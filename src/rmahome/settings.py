aa05e2696dfd6b6f08107ffecd1276ff40ee8dcc
RMA <cris7wail9@gmail.com>
2025-01-20 17:22:49 +0100

    For Deployement with Railway with Docker

--- src/rmahome/settings.py
+++ src/rmahome/settings.py
@@ -14,8 +14,15 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app'
+]
 
+if DEBUG:
+    ALLOWED_HOSTS += [
+        'localhost',
+        '127.0.0.1'
+    ]
 
 # Application definition
 
