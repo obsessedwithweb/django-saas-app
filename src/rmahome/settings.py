d0acff126ebb216c24cb9c6d7d2586b3df261012
RMA <cris7wail9@gmail.com>
2025-01-20 19:36:10 +0100

    About dotenv

--- src/rmahome/settings.py
+++ src/rmahome/settings.py
@@ -1,4 +1,4 @@
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pn+v5#lsx+vqgo9&i!w7h1p%%lo&n2l3#^0ti&q()pv_n(pm4s'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [
     '.railway.app'
