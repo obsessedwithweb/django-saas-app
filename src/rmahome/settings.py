147ad3077995753d94da89ea9be9fbfa08d04f50
RMA <cris7wail9@gmail.com>
2025-01-21 20:40:05 +0100

    Dealing with static files

--- src/rmahome/settings.py
+++ src/rmahome/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = []
+# STATIC_ROOT
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
