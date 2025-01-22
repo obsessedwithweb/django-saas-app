9a5832e4f02002d5819c3acdd57f8cee0fd4c22a
RMA <cris7wail9@gmail.com>
2025-01-22 20:04:35 +0100

    working with 'WHITENOISE'

--- src/rmahome/settings.py
+++ src/rmahome/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,11 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "local-cdn"
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
