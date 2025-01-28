15f45a179aeef951da69e46abe70e51a5736d884
RMA <cris7wail9@gmail.com>
2025-01-28 18:00:59 +0100

    Fix AllowedHosts

--- src/rmahome/settings.py
+++ src/rmahome/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [
-    '.railway.app/',
+    '.railway.app',
 ]
 
 # CSRF_COOKIE_DOMAIN = [*ALLOWED_HOSTS]
